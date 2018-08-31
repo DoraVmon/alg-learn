e643431e5e9c59655d14dc837f160b6b26b7e4c4
DoraVmon <doravmon@qq.com>
2018-09-01 01:11:34 +0800

    LeetCode 300 O(nlog(n))

--- LeetCode 300.Longest Increasing Subsequence(solve1).cpp
+++ LeetCode 300.Longest Increasing Subsequence(solve1).cpp
@@ -1,25 +1,49 @@
 #include <stdio.h>
 #include <vector>
 
+int binary_search(std::vector<int> nums, int target){
+    int index = -1;
+    int begin = 0;
+    int end = nums.size() - 1;
+    while (index == -1){
+        int mid = (begin + end) / 2;
+        if (target == nums[mid]){
+            index = mid;
+        }
+        else if (target < nums[mid]){
+            if (mid == 0 || target > nums[mid - 1]){
+                index = mid;
+            }
+            end = mid - 1;
+        }
+        else if (target > nums[mid]){
+            if (mid == nums.size() - 1 || target < nums[mid + 1]){
+                index = mid + 1;
+            }
+            begin = mid + 1;
+        }
+    }
+    return index;
+}
+
 class Solution {
 public:
     int lengthOfLIS(std::vector<int>& nums) {
 		if (nums.size() == 0) {
 			return 0;
 		}
-		std::vector<int> dp(nums.size(), 1);
-		int ret = 1;
-		for(size_t i = 0; i < dp.size(); i++) {
-			for(size_t j = 0; j < i; j++) {
-				if (nums[j] < nums[i] && dp[i] < dp[j] + 1) {
-					dp[i] = dp[j] + 1;
-				}
+		std::vector<int> st;
+		st.push_back(nums[0]);
+		for(size_t i = 1; i < nums.size(); i++) {
+			if (nums[i] > st.back()) {
+				st.push_back(nums[i]);
 			}
-			if (ret < dp[i]) {
-				ret = dp[i];
+			else {
+				int k = binary_search(st, nums[i]);
+				st[k] = nums[i];
 			}
 		}
-		return ret;
+		return st.size();
 	}
 };
 
