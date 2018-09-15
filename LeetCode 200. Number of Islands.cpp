90d109f67a20adad655a6054b4b0b4cd3ca7cb7d
DoraVmon <doravmon@qq.com>
2018-09-15 23:50:22 +0800

    edit leetcode 200 (BFS)

--- LeetCode 200. Number of Islands.cpp
+++ LeetCode 200. Number of Islands.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 #include <vector>
+#include <queue>
+
 class Solution
 {
   public:
@@ -22,7 +24,7 @@ class Solution
 			{
 				if (grid[i][j] == '1' && mark[i][j] == 0)
 				{
-					DFS(grid, mark, i, j);
+					BFS(grid, mark, i, j);
 					++ret;
 				}
 			}
@@ -31,6 +33,37 @@ class Solution
 	}
 
   private:
+	void BFS(
+		std::vector<std::vector<char> > &grid,
+		std::vector<std::vector<int> > &mark,
+		int x, int y)
+	{
+		std::queue<std::pair<int, int> > Q;
+		Q.push(std::make_pair(x, y));
+		mark[x][y] = 1;
+		static int dx[] = {-1, +1, 0, 0};
+		static int dy[] = {0, 0, -1, +1};
+		while(!Q.empty())
+		{
+			x = Q.front().first;
+			y = Q.front().second;
+			Q.pop();
+			for(int i = 0; i < 4; i++)
+			{
+				int new_x = x + dx[i];
+				int new_y = y + dy[i];
+				if (new_x < 0 || new_x >= mark.size() || new_y < 0 || new_y >= mark[new_x].size())
+				{
+					continue;
+				}
+				if (mark[new_x][new_y] == 0 && grid[new_x][new_y] == '1')
+				{
+					Q.push(std::make_pair(new_x, new_y));
+					mark[new_x][new_y] = 1;
+				}
+			}
+		}
+	}
 	void DFS(
 		std::vector<std::vector<char> > &grid,
 		std::vector<std::vector<int> > &mark,
